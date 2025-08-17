5e12ec0da38bda05b6784d6a3c7aa19427b757f2
何飞杭 <poi@hefeihangdeMacBook-Air.local>
2025-08-17 19:46:12 +0800

    2

--- multiplot_shot.py
+++ multiplot_shot.py
@@ -7,8 +7,9 @@ import matplotlib.ticker as ticker
 # 数据文件路径
 folder_path = os.path.expanduser('~/Desktop/data_EMP/')
 fn2 = os.path.join(folder_path, 'attenuate.xlsx')
-read_dir = os.path.join(folder_path, '20Au')
-save_dir = read_dir
+folder = '1000Ta'
+read_dir = os.path.join(folder_path, folder)
+save_dir = os.path.join(read_dir, 'pictures')
 
 # 延迟参数
 tdelay0 = 198.646 - 45.08 + 13.55
@@ -22,12 +23,16 @@ fs = 12.5e9
 dt = 1 / fs
 
 # 要处理的shot_id列表
-shot_id_list = [5, 10, 44, 48, 50, 57, 62, 63, 65]
+shot_id_list = [33,37,39,81,82,67]
 a_list = [1, 2, 3, 4, 5, 6]
+
+# 确保保存目录存在
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 
 for a in a_list:
     # 时域图
-    fig, axs = plt.subplots(len(shot_id_list), 1, sharex=True, figsize=(8, 16))
+    fig, axs = plt.subplots(len(shot_id_list), 1, sharex=True, sharey=True, figsize=(8, 12))
     for idx, shot_id in enumerate(shot_id_list):
         fn1 = os.path.join(read_dir, f'{shot_id:03d}.csv')
         if not os.path.isfile(fn1):
@@ -39,25 +44,29 @@ for a in a_list:
         t = M[:, 3 * a - 3] - tdelay
         signal = M[:, 3 * a - 2] * (10 ** ((attenuate[a-1] + attenuate0) / 20))
         E = 27.46 * signal
-        mask = (t >= 0) & (t <= 6e-8)
+        mask = (t >= 0) & (t <= 8e-8)
         t_sel = t[mask]
         E_sel = E[mask]
         axs[idx].plot(t_sel, E_sel, label=f'Shot {shot_id:03d}')
         axs[idx].set_ylabel(f'Shot {shot_id:03d}\nE(V/m)')
-        axs[idx].legend()
+        axs[idx].yaxis.set_major_locator(ticker.MaxNLocator(nbins=3))
+        # axs[idx].legend()
         formatter = ticker.ScalarFormatter(useMathText=True)
-        formatter.set_powerlimits((0, 0))
+        formatter.set_powerlimits((3, 3))
         axs[idx].yaxis.set_major_formatter(formatter)
-    axs[-1].set_xlabel("t(s)")
-    plt.xlim(0, 6e-8)
-    plt.suptitle(f'CH{a} 时域')
-    fn_img = os.path.join(save_dir, f'ch{a:01d}_multishot_subplot.png')
+        formatter = ticker.ScalarFormatter(useMathText=True)
+        formatter.set_powerlimits((-9, -9))
+        axs[idx].xaxis.set_major_formatter(formatter)
+    axs[-1].set_xlabel("t(ns)")
+    plt.xlim(0, 8e-8)
+    plt.suptitle(f'Electric field {a}')
+    fn_img = os.path.join(save_dir, f'{folder}ch{a}signal.png')
     plt.subplots_adjust(top=0.95, wspace=0, hspace=0)
     plt.savefig(fn_img, bbox_inches='tight', dpi=600)
     plt.close()
 
     # 傅立叶变换图
-    fig_fft, axs_fft = plt.subplots(len(shot_id_list), 1, sharex=True, figsize=(8, 16))
+    fig_fft, axs_fft = plt.subplots(len(shot_id_list), 1, sharex=True, figsize=(8, 12))
     for idx, shot_id in enumerate(shot_id_list):
         fn1 = os.path.join(read_dir, f'{shot_id:03d}.csv')
         if not os.path.isfile(fn1):
@@ -68,7 +77,7 @@ for a in a_list:
         t = M[:, 3 * a - 3] - tdelay
         signal = M[:, 3 * a - 2] * (10 ** ((attenuate[a-1] + attenuate0) / 20))
         E = 27.46 * signal
-        mask = (t >= 0) & (t <= 6e-8)
+        mask = (t >= 0) & (t <= 8e-8)
         E_sel = E[mask]
         L = len(E_sel)
         Y = np.fft.fft(E_sel)
@@ -79,14 +88,16 @@ for a in a_list:
         f = fs * np.arange(0, L // 2) / L
         axs_fft[idx].plot(f, P1, 'r', label=f'Shot {shot_id:03d}')
         axs_fft[idx].set_ylabel(f'Shot {shot_id:03d}\n|P1|')
-        axs_fft[idx].legend()
+        axs_fft[idx].yaxis.set_major_locator(ticker.MaxNLocator(nbins=3))
+        # axs_fft[idx].legend()
         formatter = ticker.ScalarFormatter(useMathText=True)
         formatter.set_powerlimits((0, 0))
         axs_fft[idx].yaxis.set_major_formatter(formatter)
+        axs_fft[idx].xaxis.set_major_formatter(formatter)
     axs_fft[-1].set_xlabel("Frequency (Hz)")
-    plt.xlim(0, 6e9)
-    plt.suptitle(f'CH{a} fft')
-    fn_img_fft = os.path.join(save_dir, f'ch{a:01d}_multishot_fft_subplot.png')
+    plt.xlim(0, 3e9)
+    plt.suptitle(f'FFT {a}')
+    fn_img_fft = os.path.join(save_dir, f'{folder}ch{a}fft.png')
     plt.subplots_adjust(top=0.95, wspace=0, hspace=0)
     plt.savefig(fn_img_fft, bbox_inches='tight', dpi=600)
     plt.close()
